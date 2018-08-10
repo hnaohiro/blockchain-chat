d2989d7fe43bf1deb91121aca151b917f188f6a8
hnaohiro <hasegawanaohiro1989@gmail.com>
2018-08-11 01:06:18 +0900

    fix contract.js

--- src/contract.js
+++ src/contract.js
@@ -1,7 +1,8 @@
 import web3 from "./web3";
 import Chat from "../build/contracts/Chat.json";
 
-const tokenAddress = "0x3a9c898af2d01af4544c67a0c87775179230f85d";
-const contract = new web3.eth.Contract(Chat.abi, tokenAddress);
+const networkId = 5777;
+const address = Chat.networks[networkId].address;
+const contract = new web3.eth.Contract(Chat.abi, address);
 
 export default contract;
